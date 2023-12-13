1d14c699bbf33a4bcaa348c174af733d54c78a9b
Ahmad Hassouneh <ahmad.hassouneh@vaimanagement.co>
2023-12-13 12:26:39 +0400

    change button style

--- components/ui/Button/styles.js
+++ components/ui/Button/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    padding: 15,
+    paddingVertical: 20,
     marginVertical: 5,
 
     alignItems: "center",
